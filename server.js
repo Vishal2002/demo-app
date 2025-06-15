b5ccaec64a0ef467fa03079e38085d88058c0660
Vishal <vishalsharma05052002@gmail.com>
2025-06-15 22:37:18 +0530

    change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({ 
-    message: 'Hello CI/CD World!', 
+    message: 'Hello changed are here  CI/CD World!', 
     version: '1.0.0'
   });
 });
